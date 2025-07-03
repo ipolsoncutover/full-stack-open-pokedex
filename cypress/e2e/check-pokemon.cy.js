5c9d64430679594ee018996eeb9d3ccc755f87ca
ipolsoncutover <157122641+ipolsoncutover@users.noreply.github.com>
2025-07-03 15:01:26 +0100

    Update check-pokemon.cy.js

--- cypress/e2e/check-pokemon.cy.js
+++ cypress/e2e/check-pokemon.cy.js
@@ -2,7 +2,7 @@ describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('/')
 
-    cy.contains('Ivysaur').click() // find a visible element with the text "Ivysaur" and click it
+    cy.contains('ivysaur').click() // find a visible element with the text "Ivysaur" and click it
 
     cy.url().should('include', '/pokemon/ivysaur')
 
